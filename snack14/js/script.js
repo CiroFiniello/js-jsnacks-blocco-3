74133d5e01c4a832a37bac55c365fd5e39ecd116
CodeKiro <kirofiniello@outlook.it>
2024-05-16 18:18:33 +0200

    snak pomeridiani

---
+++ snack14/js/script.js
@@ -0,0 +1,15 @@
+// Esercizio 1/A
+// Dichiara una funzione chiamata printHello che non prenda nessun argomento e il cui scopo è stampare in console la scritta "Hello World!"
+// function printHello(){
+//             console.log('Hello world');
+// }
+
+Esercizio 2/A
+Dichiara una funzione chiamata getOneStringFromTwo che prenda come argomenti due stringhe e ritorni come risultato un'unica stringa formata dalle due stringhe separate da uno spazio.
+function getOneStringFromTwo(string1,string2){
+    return = string1 +''+string2;
+}
+
+
+
+
