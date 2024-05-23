60efeb47d3824a1858d86b101761096120ad5811
CodeKiro <kirofiniello@outlook.it>
2024-05-23 12:14:56 +0200

    commit

---
+++ snack aut/js/script.js
@@ -0,0 +1,39 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+const garage = [
+    {
+        marca: 'audi',
+        modello: 'r8',
+        alimentazione: 'benz' ,
+    },
+    {
+        marca: 'bmw',
+        modello: 'z3',
+        alimentazione: 'benz' ,
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'golf',
+        alimentazione: 'diesel' ,
+    },  {
+        marca: 'audi',
+        modello: '80',
+        alimentazione: 'diesel' ,
+    },
+    {
+        marca: 'ford',
+        modello: 'ram',
+        alimentazione: 'benz' ,
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'golf',
+        alimentazione: 'diesel' ,
+    },
+]
+
+
+
